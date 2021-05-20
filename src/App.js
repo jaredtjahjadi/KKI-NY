457b511afdde661c415e7f3199cd75125a3dae3c
JareBear16 <jaredtjahjadi@gmail.com>
2021-05-19 22:23:46 -0400

    Modified some elements of the HTML, CSS, and JS file; deleted unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="logo.png" alt="KKI-NY logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          KKI-NY website: WIP.<br />
+          Situs web untuk KKI-NY sedang dalam proses.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a className="App-link" href="https://www.facebook.com/kkinewyork">Facebook</a>
       </header>
     </div>
   );
