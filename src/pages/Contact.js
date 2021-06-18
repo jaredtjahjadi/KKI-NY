87e3a4b48614a6bc934c5696e5fd4c6633b7cc04
JareBear16 <jaredtjahjadi@gmail.com>
2021-06-18 00:34:31 -0400

    (INCOMPLETE) Installed package that helps to send emails from Contact page. Doesn't work yet.

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,20 +1,39 @@
 import { useTranslation } from 'react-i18next';
+import nodemailer from 'nodemailer';
+
+function sendMail() {
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.PASS
+        }
+    });
+    var message = {
+        from: `${document.getElementById('email').value}`,
+        to: process.env.EMAIL,
+        subject: "Test",
+        text: `${document.getElementById('inquiry').value}`
+    }
+    transporter.sendMail(message);
+}
 
 export default function Contact() {
     const { t } = useTranslation();
     document.title = `Contact Us | ${t('name')}`;
+
     return (
         <div className="content">
-            <form action="contact.php">
+            <form>
                 <label for="firstname">{t('contact.firstname')}</label>
-                <input type="text" placeholder={t('contact.firstname')} /><br />
+                <input type="text" id="firstname" placeholder={t('contact.firstname')} /><br />
                 <label for="lastname">{t('contact.lastname')}</label>
-                <input type="text" placeholder={t('contact.lastname')} /><br />
+                <input type="text" id="lastname" placeholder={t('contact.lastname')} /><br />
                 <label for="email">{t('contact.email')}</label>
-                <input type="text" placeholder={t('contact.email')} /><br />
+                <input type="text" id="email" placeholder={t('contact.email')} /><br />
                 <label for="inquiry">{t('contact.inquiry')}</label>
-                <textarea type="text" placeholder="How can we help you?" /><br />
-                <input type="submit" value={t('contact.submit')} />
+                <textarea type="text" id="inquiry" placeholder="How can we help you?" /><br />
+                <input type="submit" value={t('contact.submit')} onClick={sendMail} />
             </form>
         </div>
     );
