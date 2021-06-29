dacf6022fd4cae9d424777807cf9e16c7412c9cd
JareBear16 <jaredtjahjadi@gmail.com>
2021-06-28 23:29:41 -0400

    The slideshow slideshows now! Doesn't look pretty tho

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -1,40 +1,17 @@
-//To be displayed on the homepage
-
+import { useState, useEffect } from "react";
 export default function Slideshow() {
-    // var slideIndex = 1;
-    // const showSlides = (n) => {
-    //     var i;
-    //     var slides = document.getElementsByClassName("slides");
-    //     var dots = document.getElementsByClassName("dots");
-    //     if(n > slides.length) slideIndex = 1
-    //     if(n < 1) slideIndex = slides.length
-    //     for(i = 0; i < slides.length; i++) slides[i].style.display = "none";
-    //     for(i = 0; i < dots.length; i++) dots[i].className = dots[i].className.replace(" active", "");
-    //     slides[slideIndex - 1].style.display = "block";
-    //     dots[slideIndex - 1].className += "active";
-    // }
-    // showSlides(slideIndex);
-    // const plusSlides = (n) => showSlides(slideIndex += n);
-    // const currSlide = (n) => showSlides(slideIndex = n);
-    return (
-        <div>
-            (Slideshow WIP)
-            <div class="slideshow">
-                <div class="slides fade">
-                    <img src='img/pic1.png' style={{width:'100%'}} alt="Pic 1" />
-                    <div class="text">Pic 1</div>
-                </div>
-                <div class="slides fade">
-                    <img src='img/pic2.png' style={{width:'100%'}} alt="Pic 2" />
-                    <div class="text">Pic 2</div>
-                </div>
-                {/* <a class="prev" onclick={plusSlides(-1)} href='/#'>&#10094;</a>
-                <a class="next" onclick={plusSlides(-1)} href='/#'>&#10095;</a> */}
-            </div>
-            <div style={{'text-align': 'center'}}>
-                {/* <span class="dot" onClick={currSlide(1)} />
-                <span class="dot" onClick={currSlide(2)} /> */}
+    let imgs = ['img/pic1.png', 'img/pic2.png'];
+    const [index, setIndex] = useState(0);
+    useEffect(() => {setIndex(0)}, []);
+    const next = () => {(index === imgs.length - 1) ? setIndex(0) : setIndex(index + 1)};
+    const prev = () => {(index === 0) ? setIndex(imgs.length - 1) : setIndex(index - 1)};
+    return(
+        <div className="slideshow">
+            <img className="main" src={imgs[index]} alt="Pic 1" />
+            <div className="actions">
+                <button onClick={prev}>👈</button>
+                <button onClick={next}>👉</button>
             </div>
         </div>
-    );
+    )
 }
