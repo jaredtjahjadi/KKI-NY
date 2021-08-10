31f2ca8f91624b3ab532f2917704ef46712ce76d
JareBear16 <jaredtjahjadi@gmail.com>
2021-08-09 23:38:21 -0400

    Fixed bug that caused text to appear after scrolling past hero container. Tried changing navbar so that it's responsive (no ugliness on mobile) but reeeeee CSS is so annoying that my eyes started hurting dude

--- src/components/HeroImage.js
+++ src/components/HeroImage.js
@@ -1,7 +1,5 @@
 export default function HeroImage() {
     return (
-        <div className="hero-container">
-            <img alt="Hero" src={'../images/pic1.png'} />
-        </div>
+        <div className="hero-container" />
     );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,25 +1,36 @@
 import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
+import Hamburger from 'hamburger-react';
 
 export default function NavBar() {
     const { t } = useTranslation();
-    const [click, setClick, navBar, setNavBar] = useState(false);
-    const [button, setButton] = useState(true);
-
-    const handleClick = () => setClick(!click);
+    const [click, setClick] = useState(false);
     const closeMenu = () => setClick(false);
-
-    const showButton = () => { (window.innerWidth <= 960) ? setButton(false) : setButton(true); }
-    window.addEventListener('resize', showButton);
+    const handleClick = () => setClick(!click);
 
     return (
         <nav className="NavBar">
-            <NavLink to='/' className="Link" onClick={closeMenu}>{t('pages.home')}</NavLink>
-            <NavLink to='/about' className="Link" onClick={closeMenu}>{t('pages.about')}</NavLink>
-            <NavLink to='/activities' className="Link" onClick={closeMenu}>{t('pages.activities')}</NavLink>
-            <NavLink to="/newsletter" className="Link" onClick={closeMenu}>{t('pages.newsletter')}</NavLink>
-            <NavLink to='/contact' className="Link" onClick={closeMenu}>{t('pages.contact')}</NavLink>
+            <div className="icon" onClick={handleClick}>
+                <Hamburger toggled={click} toggle={setClick} />
+            </div>
+            <ul className={click ? 'Links active' : 'Links'}>
+                <li className="nav-elem">
+                    <NavLink to='/' className="Link" onClick={closeMenu}>{t('pages.home')}</NavLink>
+                </li>
+                <li className="nav-elem">
+                    <NavLink to='/about' className="Link" onClick={closeMenu}>{t('pages.about')}</NavLink>
+                </li>
+                <li className="nav-elem">
+                    <NavLink to='/activities' className="Link" onClick={closeMenu}>{t('pages.activities')}</NavLink>
+                </li>
+                <li className="nav-elem">
+                    <NavLink to="/newsletter" className="Link" onClick={closeMenu}>{t('pages.newsletter')}</NavLink>
+                </li>
+                <li className="nav-elem">
+                    <NavLink to='/contact' className="Link" onClick={closeMenu}>{t('pages.contact')}</NavLink>
+                </li>
+            </ul>
         </nav>
     );
 }
